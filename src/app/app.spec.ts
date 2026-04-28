8c1b2fbaf20e618a8dd22c0b50fede24885f8072
Raonhn <nathanraoliveira@hotmail.com>
2026-04-27 23:41:53 -0300

    fix: Ajuste para ci rodar

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -14,10 +14,13 @@ describe('App', () => {
     expect(app).toBeTruthy();
   });
 
-  it('should render title', async () => {
+  it('should render the home hero title', async () => {
     const fixture = TestBed.createComponent(App);
+    fixture.detectChanges();
     await fixture.whenStable();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, mintly-front-end');
+    expect(compiled.querySelector('h1')?.textContent).toContain(
+      'Assistente financeiro moderno para aut',
+    );
   });
 });
